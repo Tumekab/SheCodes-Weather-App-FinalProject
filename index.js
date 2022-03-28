0065f915d5a35e5770576b36af3cddff507cbf16
Tumeka Bursto <tumeka.burston@cxpartners.co.uk>
2022-03-28 16:01:57 +0100

    api integration for forecast days and weather

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-let apiKey = "60ace44fa1b88492bab7a31edc3708f3";
-let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
-// `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+
 
 function formatTime(timestamp) {
   let date = new Date(timestamp);
@@ -30,6 +28,50 @@ function formatDay(timestamp) {
   return day;
 }
 
+function displayForecast(response) {
+    let forecast = response.data.daily;
+    let weather = response.data.current.weather[0].description;
+    
+  
+    let forecastElement = document.querySelector("#forecast");
+    // let weatherElement = document.querySelector("#current-weather");
+  
+    let forecastHTML = `<div class="row week-forecast">`;
+    forecast.forEach(function (forecastDay, index) {
+      if (index < 6) {
+        forecastHTML =
+          forecastHTML +
+          `
+        <div class="col">
+          <h3>${formatDay(forecastDay.dt)}</h3>
+          <img
+            src="http://openweathermap.org/img/wn/${
+              forecastDay.weather[0].icon
+            }@2x.png"
+            alt=""
+            width="42"
+          />
+          <p class="weather">${weather}</p>
+          <div class="weather-forecast-temperatures">
+            <span> ${Math.round(
+              forecastDay.temp.day
+            )}° </span>
+          </div>
+        </div>
+    `;
+      }
+    });
+  
+    forecastHTML = forecastHTML + `</div>`;
+    forecastElement.innerHTML = forecastHTML;
+  }
+
+function getForecast(coordinates) {
+    let apiKey = "60ace44fa1b88492bab7a31edc3708f3";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayForecast);
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#current-temperature");
   let cityElement = document.querySelector("#searched-city");
@@ -49,7 +91,11 @@ function displayTemperature(response) {
   windElement.innerHTML = Math.round(response.data.wind.speed);
   timeElement.innerHTML = formatTime(response.data.dt * 1000);
   dayElement.innerHTML = formatDay(response.data.dt);
+
+  getForecast(response.data.coord);
+
 }
+
 
 function searchCity(city) {
   let apiKey = "60ace44fa1b88492bab7a31edc3708f3";
@@ -93,3 +139,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemp);
 
 searchCity("London");
+displayForecast();
