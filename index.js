ace805ed862e849160b2305521c0c825c876582f
Tumeka Bursto <tumeka.burston@cxpartners.co.uk>
2022-03-28 14:44:01 +0100

    sorted out the fahrenheit / celsius change

--- index.js
+++ index.js
@@ -38,7 +38,11 @@ function displayTemperature(response) {
   // let windElement =  document.querySelector("#wind");
   let timeElement = document.querySelector("#current-time");
   let dayElement = document.querySelector("#current-day");
-  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  
+  celsiusTemperature = response.data.main.temp;
+
+
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   // humidityElement.innerHTML = response.data.main.humidity;
@@ -47,18 +51,45 @@ function displayTemperature(response) {
   dayElement.innerHTML = formatDay(response.data.dt);
 }
 
-function search(city) {
+function searchCity(city) {
   let apiKey = "60ace44fa1b88492bab7a31edc3708f3";
   let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiURL).then(displayTemperature);
 }
 
 function handleSubmit(event) {
-  event.preventDefault();
-  let searchInputElement = document.querySelector("#search-input");
-  search(searchInputElement.value);
+    event.preventDefault();
+    let searchInputElement = document.querySelector("#search-input");
+    searchCity(searchInputElement.value);
 }
+
+function displayFahrenheitTemp(event) {
+    event.preventDefault();
+    let fahrenheitTemp = (celsiusTemperature * 9 / 5 + 32);
+    celsiusLink.classList.remove("active");
+    fahrenheitLink.classList.add("active");
+    let temperatureElement = document.querySelector("#current-temperature");
+    temperatureElement.innerHTML = Math.round(fahrenheitTemp);
+}
+
+function displayCelsiusTemp(event) {
+    event.preventDefault();
+    let temperatureElement = document.querySelector("#current-temperature");
+    celsiusLink.classList.add("active");
+    fahrenheitLink.classList.remove("active");
+    temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+
+let celsiusTemperature = null;
+
+
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
 
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemp);
 
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemp);
+
+searchCity("London");
